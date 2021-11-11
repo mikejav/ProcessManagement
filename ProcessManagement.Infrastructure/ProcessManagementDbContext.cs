293eb458d9570b8361d4d02df3be8254faa61b18
mikejav <mikejav@users.noreply.github.com>
2021-11-11 18:53:29 +0100

    add DB Context

---
+++ ProcessManagement.Infrastructure/ProcessManagementDbContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProcessManagement.Core.Entities;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
+namespace ProcessManagement.Infrastructure
+{
+    public class ProcessManagementDbContext : DbContext
+    {
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<WorkItem> WorkItems { get; set; }
+        public ProcessManagementDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Project>(eb => {
+                eb.HasKey(p => p.Id);
+                eb.Property(p => p.Id)
+                    .ValueGeneratedOnAdd();
+
+                eb.Property(p => p.Name)
+                    .IsRequired(true)
+                    .HasMaxLength(256);
+
+                eb.Property(p => p.Description)
+                    .IsRequired(false)
+                    .HasColumnType("TEXT");
+            });
+
+            modelBuilder.Entity<WorkItem>(wi => {
+                wi.HasOne(w => w.Project).WithMany();
+
+                wi.Property(p => p.Id)
+                    .ValueGeneratedOnAdd();
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
